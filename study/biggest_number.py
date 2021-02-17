4607ba50cf0dd1610393a3b6c3ffd9746b69dff6
clastro <37033795+clastro@users.noreply.github.com>
2021-02-17 14:40:05 +0900

    Update biggest_number.py

--- study/biggest_number.py
+++ study/biggest_number.py
@@ -9,3 +9,14 @@ def solution(array):
     num_list = list(map(int, num_list))
     result = str(max(num_list))
     return result
+
+# 100점 중 27.3점 코드
+# [40,403] 의 경우 40403이 되어야 하는데 40340이 되므로 수정이 필요하다
+
+def solution(array):
+    new_list = [i/10 if len(str(i)) == 2 else i/100 if len(str(i)) == 3 else i/1000 if len(str(i)) == 4 else i for i in array ]
+    sorted_list = sorted(new_list,reverse=True)
+    result = ''.join([str(elem) for elem in sorted_list]).replace('.','')
+    return result
+
+
